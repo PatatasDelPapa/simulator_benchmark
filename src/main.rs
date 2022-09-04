04d43fe5ee09920acdf9eb7ae202e3a35d59fe51
PatatasDelPapa <43626645+PatatasDelPapa@users.noreply.github.com>
2022-09-04 10:20:13 -0300

    Cleanup main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,5 @@
-use std::time::Duration;
-
 use simulator_benchmark::simulation;
-use simulator_benchmark::test_simulation;
 
 fn main() {
     simulation(50000);
-}
-
-#[allow(dead_code)]
-fn testing_simulator() {
-    let (mut simulation, count_key, shared_state) = test_simulation();
-    let limit = Duration::from_secs(1000);
-    simulation.run_with_limit(limit);
-
-    // let shared_state = simulation.state();
-    let mut state = shared_state.take();
-    // dbg!(&state);
-    let count = state.remove(count_key).unwrap();
-    println!("Final count = {}", count);
-    println!("Final time = {:?}", simulation.time());
 }
